665e84853e78dcf8ddb6b8d72f88d6b113c68766
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-03-20 18:17:07 +0900

    Add files via upload

---
+++ 25일차 - filter, bindingresult, mybatis 설정/MemberDAO3.java
@@ -0,0 +1,64 @@
+package sku.lesson.spring.dao;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import sku.lesson.spring.dto.MemberDTO;
+
+@Repository
+public class MemberDAO3 {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public boolean insert(MemberDTO member) {
+		boolean flag = false;
+		int result = sqlSession.insert("mapper.member.insert");
+		if(result > 0) flag = true;
+		return flag;
+	}
+
+	public ArrayList<MemberDTO> selectAll() {
+		// TODO Auto-generated method stub
+		ArrayList<MemberDTO> list = null;
+		list = (ArrayList)sqlSession.selectList("mapper.member.selectAll");
+		return list;
+	}
+	
+	//search
+	public MemberDTO select(String id) {
+		MemberDTO dto = null;
+		dto = sqlSession.selectOne("mapper.member.select");
+		return dto;
+	}
+	//update
+	public boolean update(MemberDTO dto) {
+		boolean flag = false;
+		int result = sqlSession.update("mapper.member.update");
+		if(result > 0) flag = true;
+		return flag;
+	}
+	//delete
+	public boolean delete(String id) {
+		boolean flag = false;
+		int result = sqlSession.delete("mapper.member.delete");
+		if(result > 0) flag = true;
+		return flag;
+	}
+	
+	public boolean selectById(String id) {
+		boolean flag = false;
+		int cnt = sqlSession.selectOne("mapper.member.selectById");
+		if(cnt > 0) flag = true;
+		return flag;
+	}
+}
+
+
+
+
+
+
